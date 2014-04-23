33e18978f0652840e8c6402fc7392b825e426ec8
Gregory Cornelius <gcorne@gmail.com>
2014-04-23 09:50:59 -0400

    Adjust the layout.
    Change order to top, bottom, left, right.
    Remove (px).

--- _inc/advanced-image-styles-tmpl.php
+++ _inc/advanced-image-styles-tmpl.php
@@ -2,7 +2,7 @@
 	<span class="group-label"><?php esc_html_e( 'Image Border', 'advanced-image-styles' ); ?></span>
 	<div class="image-settings-group">
 		<label class="border-width">
-			<span><?php esc_html_e( 'Width', 'advanced-image-styles' ); ?> <small>(px)</small></span>
+			<span><?php esc_html_e( 'Width', 'advanced-image-styles' ); ?></span>
 			<input type="text" data-setting="borderWidth" value="{{ data.borderWidth }}" />
 		</label>
 		<label class="border-color">
@@ -12,21 +12,21 @@
 	</div>
 	<span class="group-label"><?php esc_html_e( 'Image Margins', 'advanced-image-styles' ); ?></span>
 	<div class="image-settings-group margins">
-		<label class="left">
-			<span><?php esc_html_e( 'Left', 'advanced-image-styles' ); ?></span>
-			<input type="text" data-setting="marginLeft" value="{{ data.marginLeft }}" />
-		</label>
 		<label class="top">
 			<span><?php esc_html_e( 'Top', 'advanced-image-styles' ); ?></span>
 			<input type="text" data-setting="marginTop" value="{{ data.marginTop }}" />
 		</label>
-		<label class="right">
-			<span><?php esc_html_e( 'Right', 'advanced-image-styles' ); ?></span>
-			<input type="text" data-setting="marginRight" value="{{ data.marginRight }}" />
-		</label>
 		<label class="bottom">
 			<span><?php esc_html_e( 'Bottom', 'advanced-image-styles' ); ?></span>
 			<input type="text" data-setting="marginBottom" value="{{ data.marginBottom }}" />
+		</label>
+		<label class="left">
+			<span><?php esc_html_e( 'Left', 'advanced-image-styles' ); ?></span>
+			<input type="text" data-setting="marginLeft" value="{{ data.marginLeft }}" />
+		</label>
+		<label class="right">
+			<span><?php esc_html_e( 'Right', 'advanced-image-styles' ); ?></span>
+			<input type="text" data-setting="marginRight" value="{{ data.marginRight }}" />
 		</label>
 	</div>
 </script>
